6e4192054d1a810babd3e16b2845ba695f0f6008
arpitkumar93 <arpit.kumar93@gmail.com>
2017-04-09 22:40:22 +0530

    Another Client/Server Program

---
+++ validation.php
@@ -0,0 +1,11 @@
+<h1>You are a valid user..!!</h1>
+<?php
+
+	echo "Name:", $_GET['name'];  ?>  <br> <?php  
+	echo "Address:",$_GET['address'];?>  <br> <?php  
+	echo "Gender:",$_GET['gender'];?>  <br> <?php  
+	echo "DOB:",$_GET['pday']; ?>  <br> <?php
+	echo "Email:",$_GET['email'];  ?>  <br> <?php
+	echo "Mobile No:",$_GET['mobile'];
+
+?>
